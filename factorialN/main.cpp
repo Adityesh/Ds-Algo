f4e51aa8f65c19deee150b1f7c1b74e13eb9588a
Adityesh <adityeshmishra98@gmail.com>
2019-10-10 23:07:02 +0530

    FactN

---
+++ factorialN/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int factorial(int n){
+
+if(n ==1){
+    return 1;
+}
+
+else
+{
+    return n * factorial(n-1);
+
+}
+
+
+
+}
+
+int main()
+{
+    cout << factorial(3);
+    return 0;
+
+}
